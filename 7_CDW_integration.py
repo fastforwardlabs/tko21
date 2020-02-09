734601b9736e48852996c86c73359c1b6504200e
Trial User Nineteen <trialuser19@trycdp.com>
2020-02-09 18:20:05 +0000

    mostly working

--- 7_CDW_integration.py
+++ 7_CDW_integration.py
@@ -9,27 +9,24 @@ JDBC_URL=VW_URL+';user='+JDBC_USERNAME+';password='+JDBC_PW
 spark = SparkSession\
     .builder\
     .appName("CDW Test")\
-    .config("spark.jars", "/etc/spark/jars/hive-warehouse-connector-assembly-1.0.0.7.1.0.0-565.jar")\
     .config("spark.security.credentials.hiveserver2.enabled","false ")\
     .config("spark.datasource.hive.warehouse.read.via.llap","false")\
     .config("spark.datasource.hive.warehouse.read.jdbc.mode", "client")\
     .config("spark.sql.hive.hiveserver2.jdbc.url", JDBC_URL)\
     .getOrCreate()
- 
-os.environ["PYTHONPATH"] += os.pathsep + '/etc/spark/python/lib/pyspark_hwc-1.0.0.7.1.0.0-565.zip'
+    
+#.config("spark.jars", "/etc/spark/jars/hive-warehouse-connector-assembly-1.0.0.7.1.0.0-565.jar")\
+#os.environ["PYTHONPATH"] += os.pathsep + '/etc/spark/python/lib/pyspark_hwc-1.0.0.7.1.0.0-565.zip'
 
-sys.path.append(0, '/Users/stevej/test_mod.zip')
-import test_mod
+sys.path.append('/etc/spark/python/lib/pyspark_hwc-1.0.0.7.1.0.0-565.zip')
 
-from etc.spark.python.lib import 'pyspark_hwc-1.0.0.7.1.0.0-565'
-
-#from pyspark_llap.sql.session import HiveWarehouseSession
+from pyspark_llap.sql.session import HiveWarehouseSession
 hive = HiveWarehouseSession.session(spark).build()
 hive.showDatabases().show()
 hive.showTables().show()
-hive.setDatabase("airline_ontime_orc")
-hive.sql("select code from airlines").show()
-hive.sql("select * from airports").show()  
+hive.setDatabase("default")
+hive.sql("select code from airlines_new_orc").show()
+hive.sql("select * from smaller_flight_table").show()  
 
 #SELECT * FROM smaller_flight_table LIMIT 10;
 
