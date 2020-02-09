7063c45062dc89d937f8522dac142b7f438e954a
mpgreg <systest@nightly-cdsw-4.vpc.cloudera.com>
2020-02-09 07:45:47 -0800

    push to docker hub

--- utils/build-engine.sh
+++ utils/build-engine.sh
@@ -4,8 +4,17 @@
 
 ENGINE_NAME='engine'
 ENGINE_INSTANCE='11-cml1.4-tko'
-ENGINE_FILE=${ENGINE_NAME}-${ENGINE_INSTANCE}.tar
+DOCKER_REPO='cdsw'
 
 sudo docker build --network=host -t $ENGINE_NAME:$ENGINE_INSTANCE . -f Dockerfile
-sudo docker image save -o ./$ENGINE_FILE $ENGINE_NAME:$ENGINE_INSTANCE
-sudo chmod 755 ./$ENGINE_FILE
+
+#use the following lines if you want to save and distribute the engine to CDSW servers
+#ENGINE_FILE=${ENGINE_NAME}-${ENGINE_INSTANCE}.tar
+#sudo docker image save -o ./$ENGINE_FILE $ENGINE_NAME:$ENGINE_INSTANCE
+#sudo chmod 755 ./$ENGINE_FILE
+
+#use the following lines to push the engine to the Docker Hub repo.
+IMAGE_ID=`sudo docker images | awk -v name="$ENGINE_NAME" -v tag="$ENGINE_INSTANCE" -F" " '$1==name && $2==tag { print $3 }'`
+sudo docker tag $IMAGE_ID ${DOCKER_REPO}/${ENGINE_NAME}:${ENGINE_INSTANCE}
+sudo docker login
+sudo docker push  ${DOCKER_REPO}/${ENGINE_NAME}
