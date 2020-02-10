4deff6cee47e54eafd4ce5feb9315abfba4b8b2c
root <root@nightly-cdsw-4.vpc.cloudera.com>
2020-02-10 02:34:02 -0800

    using vamsees hwc image

--- utils/build-engine.sh
+++ utils/build-engine.sh
@@ -3,18 +3,18 @@
 #sudo yum install docker
 
 ENGINE_NAME='engine'
-ENGINE_INSTANCE='11-cml1.4-tko'
+ENGINE_TAG='11-cml1.4-demo'
 DOCKER_REPO='cdsw'
 
-sudo docker build --network=host -t $ENGINE_NAME:$ENGINE_INSTANCE . -f Dockerfile
+sudo docker build --network=host -t $ENGINE_NAME:$ENGINE_TAG . -f Dockerfile
 
 #use the following lines if you want to save and distribute the engine to CDSW servers
-#ENGINE_FILE=${ENGINE_NAME}-${ENGINE_INSTANCE}.tar
-#sudo docker image save -o ./$ENGINE_FILE $ENGINE_NAME:$ENGINE_INSTANCE
+#ENGINE_FILE=${ENGINE_NAME}-${ENGINE_TAG}.tar
+#sudo docker image save -o ./$ENGINE_FILE $ENGINE_NAME:$ENGINE_TAG
 #sudo chmod 755 ./$ENGINE_FILE
 
 #use the following lines to push the engine to the Docker Hub repo.
-IMAGE_ID=`sudo docker images | awk -v name="$ENGINE_NAME" -v tag="$ENGINE_INSTANCE" -F" " '$1==name && $2==tag { print $3 }'`
-sudo docker tag $IMAGE_ID ${DOCKER_REPO}/${ENGINE_NAME}:${ENGINE_INSTANCE}
+IMAGE_ID=`sudo docker images | awk -v name="$ENGINE_NAME" -v tag="$ENGINE_TAG" -F" " '$1==name && $2==tag { print $3 }'`
+sudo docker tag $IMAGE_ID ${DOCKER_REPO}/${ENGINE_NAME}:${ENGINE_TAG}
 sudo docker login
-sudo docker push  ${DOCKER_REPO}/${ENGINE_NAME}
+sudo docker push  ${DOCKER_REPO}/${ENGINE_NAME}:${ENGINE_TAG}
